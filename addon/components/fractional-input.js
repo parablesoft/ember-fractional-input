54dcb5fd194962e1d10724bd0f0e1aec9bdcbcab
Bradley Wittenbrook <bradley.wittenbrook@gmail.com>
2018-09-12 14:42:34 -0400

    Upgrade selectize and get eslint passing

--- addon/components/fractional-input.js
+++ addon/components/fractional-input.js
@@ -6,76 +6,76 @@ import layout from '../templates/components/fractional-input';
 
 export default Component.extend({
   layout,
-  classNames:["ember-fractional-input"],
+  classNames: ["ember-fractional-input"],
   "wrapper-class": "form-group",
   "input-class": "form-control",
   placeholder: 'Enter your value',
-  actions:{
-    blurred(){
-      if(this.attrs["on-blur"]){
-	this.attrs["on-blur"]();
+  actions: {
+    blurred() {
+      if (this.get("on-blur")) {
+        this.get("on-blur")()
       }
     }
   },
   denominator: 16,
-  fractions: computed(function(){
+  fractions: computed(function () {
     let denominator = this.get("denominator");
     let result = A();
-    for(let i = 1; i < denominator; i++){
-      let displayValue = this.reduceFraction(i,denominator);
+    for (let i = 1; i < denominator; i++) {
+      let displayValue = this.reduceFraction(i, denominator);
 
-      result.pushObject({value: parseFloat(i/denominator), display: `${displayValue[0]}/${displayValue[1]}`});
+      result.pushObject({ value: parseFloat(i / denominator), display: `${displayValue[0]}/${displayValue[1]}` });
     }
     return result;
   }),
 
   valueWhole: computed({
-    get(){
+    get() {
       return this.getWhole("value");
     },
-    set(key,value){
-      return this.setWhole("value",value);
+    set(key, value) {
+      return this.setWhole("value", value);
     }
   }),
   valueFraction: computed({
-    get(){
+    get() {
       return this.getFraction("value");
     },
-    set(key,value){
-      return this.setFraction("value",value);
+    set(key, value) {
+      return this.setFraction("value", value);
     }
   }),
-  getWhole(key){
+  getWhole(key) {
     let fullNumber = this.get(key);
-    if(fullNumber===undefined)
+    if (fullNumber === undefined)
       return "";
 
     return parseInt(fullNumber);
   },
-  setWhole(key,value){
+  setWhole(key, value) {
     let calcValue = this.calcValue(value);
     let fraction = this.calcValue(this.get(`${key}Fraction`));
     this.set(key, parseFloat(calcValue) + parseFloat(fraction));
     return value;
   },
-  getFraction(key){
+  getFraction(key) {
     let fraction = this.get(key);
-    return  fraction - parseInt(fraction);
+    return fraction - parseInt(fraction);
   },
-  setFraction(key,value){
+  setFraction(key, value) {
     let calcValue = this.calcValue(value);
     let whole = this.calcValue(this.get(`${key}Whole`));
     this.set(key, parseFloat(whole) + parseFloat(calcValue));
     return value;
   },
-  calcValue(value){
+  calcValue(value) {
     return isEmpty(value) || isNaN(value) ? 0 : value;
   },
-  reduceFraction(numerator,denominator){
-    var gcd = function gcd(a,b){
-      return b ? gcd(b, a%b) : a;
+  reduceFraction(numerator, denominator) {
+    var gcd = function gcd(a, b) {
+      return b ? gcd(b, a % b) : a;
     };
-    gcd = gcd(numerator,denominator);
-    return [numerator/gcd, denominator/gcd];
+    gcd = gcd(numerator, denominator);
+    return [numerator / gcd, denominator / gcd];
   }
 });
--- tests/integration/components/fractional-input-test.js
+++ tests/integration/components/fractional-input-test.js
@@ -1,23 +1,25 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | fractional input', function(hooks) {
+module('Integration | Component | fractional input', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    await render(hbs`{{fractional-input}}`);
+  test('it renders', async function (assert) {
+    await render(hbs `{{fractional-input}}`);
 
-    assert.dom('*').hasText('');
+    assert.dom('*')
+      .hasText('');
 
     // Template block usage:
-    await render(hbs`
+    await render(hbs `
       {{#fractional-input}}
         template block text
       {{/fractional-input}}
     `);
 
-    assert.dom('*').hasText('template block text');
+    assert.dom('*')
+      .hasText('template block text');
   });
 });
